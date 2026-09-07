a9e070ffd668ecd969f4f2b47a3e17ffb3bf9ad7
Kenji Fukushima <kfuku52@gmail.com>
2026-09-08 07:43:58 +0900

    Bump version to 1.14.11

--- csubst/__init__.py
+++ csubst/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.14.10'
+__version__ = '1.14.11'
 
 # These compatibility attributes remain available when the optional compiled
 # extensions have not been built. Internal modules load extensions lazily.
